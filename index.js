4b19b2ba72674a2b2e2c409cd1f55930fd9ee6f1
Ahmed Ehab <ahmede964@gmail.com>
2025-06-07 16:01:11 +0300

    add support for vercel deploy
    Signed-off-by: Ahmed Ehab <ahmede964@gmail.com>

---
+++ index.js
@@ -0,0 +1,16 @@
+const jsonServer = require("json-server");
+const auth = require("json-server-auth");
+const path = require("path");
+
+const server = jsonServer.create();
+const router = jsonServer.router(path.join(__dirname, "db.json"));
+const middlewares = jsonServer.defaults();
+const rules = path.join(__dirname, "routes.json");
+
+server.db = router.db;
+server.use(middlewares);
+server.use(jsonServer.rewriter(require(rules)));
+server.use(auth);
+server.use(router);
+
+module.exports = server;
