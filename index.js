899a58119dde420fa5da2bbe2f7f111fc0cc6944
Ahmed Ehab <ahmede964@gmail.com>
2025-06-07 16:22:44 +0300

    add cors
    Signed-off-by: Ahmed Ehab <ahmede964@gmail.com>

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const jsonServer = require("json-server");
 const auth = require("json-server-auth");
 const path = require("path");
+const cors = require("cors");
 
 const server = jsonServer.create();
 const router = jsonServer.router(path.join(__dirname, "db.json"));
@@ -8,6 +9,7 @@ const middlewares = jsonServer.defaults();
 const rules = path.join(__dirname, "routes.json");
 
 server.db = router.db;
+server.use(cors());
 server.use(middlewares);
 server.use(jsonServer.rewriter(require(rules)));
 server.use(auth);
