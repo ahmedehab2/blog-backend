2c032b202b9daa1b2e1a956e47d379a574262a6e
Ahmed Ehab <ahmede964@gmail.com>
2025-06-09 18:08:18 +0300

    add validate middleware
    Signed-off-by: Ahmed Ehab <ahmede964@gmail.com>

--- src/validators/postValidator.js
+++ src/validators/postValidator.js
@@ -1,16 +1,20 @@
 import { body, param } from "express-validator";
+import { validate } from "../middleware/validators.js";
 
 export const createPostValidator = [
   body("title").notEmpty().withMessage("Title is required"),
   body("content").notEmpty().withMessage("Content is required"),
+  validate,
 ];
 
 export const updatePostValidator = [
   param("id").isMongoId().withMessage("Invalid post ID"),
   body("title").optional().notEmpty().withMessage("Title cannot be empty"),
   body("content").optional().notEmpty().withMessage("Content cannot be empty"),
+  validate,
 ];
 
 export const getPostByIdValidator = [
   param("id").isMongoId().withMessage("Invalid post ID"),
+  validate,
 ];
