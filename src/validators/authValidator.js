44e32454bbfeabab93f427d1106475a726213ff5
Ahmed Ehab <ahmede964@gmail.com>
2025-06-10 16:12:49 +0300

    add name length
    Signed-off-by: Ahmed Ehab <ahmede964@gmail.com>

--- src/validators/authValidator.js
+++ src/validators/authValidator.js
@@ -2,7 +2,11 @@ import { body } from "express-validator";
 import { validate } from "../middleware/validators.js";
 
 export const registerValidator = [
-  body("name").notEmpty().withMessage("Name is required"),
+  body("name")
+    .notEmpty()
+    .withMessage("Name is required")
+    .isLength({ min: 3 })
+    .withMessage("Name must be at least 3 characters long"),
   body("email").isEmail().withMessage("Invalid email address"),
   body("password")
     .isLength({ min: 6 })
