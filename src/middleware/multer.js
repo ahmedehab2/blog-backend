36bac04535a74261e043eab073114b131c1acfbe
Ahmed Ehab <ahmede964@gmail.com>
2025-06-10 16:08:56 +0300

    add limit for file upload
    Signed-off-by: Ahmed Ehab <ahmede964@gmail.com>

--- src/middleware/multer.js
+++ src/middleware/multer.js
@@ -1,9 +1,10 @@
 import multer from "multer";
 
 const storage = multer.memoryStorage();
+const limits = { fileSize: 1 * 1024 * 1024 }; // 1MB limit
 
 function uploadMiddleware(fileName) {
-  return multer({ storage }).single(fileName);
+  return multer({ storage, limits }).single(fileName);
 }
 
 export default uploadMiddleware;
