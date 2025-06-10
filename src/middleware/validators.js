e20b30457a00629b47e2577758d582d2d171eaf4
Ahmed Ehab <ahmede964@gmail.com>
2025-06-10 16:12:21 +0300

    remove request validation matched data breaks express 5 req is read only rule , pass error to error middleware
    Signed-off-by: Ahmed Ehab <ahmede964@gmail.com>

--- src/middleware/validators.js
+++ src/middleware/validators.js
@@ -1,14 +1,11 @@
-import { validationResult, matchedData } from "express-validator";
+import { matchedData, validationResult } from "express-validator";
 
 export const validate = (req, res, next) => {
   const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.status(400).json({ errors: errors.array() });
-  }
 
-  req.body = matchedData(req, { locations: ["body"] });
-  req.params = matchedData(req, { locations: ["params"] });
-  req.query = matchedData(req, { locations: ["query"] });
+  if (!errors.isEmpty()) {
+    return next(errors.formatWith(({ msg }) => msg).array());
+  }
 
   next();
 };
